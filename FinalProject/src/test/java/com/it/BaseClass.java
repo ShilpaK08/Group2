8ea0872cdf840814ec87f0a67de033f8a2cb2123
KapaVenataPrathibhaReddy <kvprathibha@gmail.com>
2021-04-19 12:11:51 +0200

    Adding the final Project

---
+++ FinalProject/src/test/java/com/it/BaseClass.java
@@ -0,0 +1,42 @@
+package com.it;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseClass {
+
+    static WebDriver driver;
+
+    static JavascriptExecutor js;
+
+
+    @BeforeEach
+    public void openUp(){
+        //open the Chrome webdriver and maximize the window before performing each test case
+        driver = new ChromeDriver();
+        driver.get("http://40.76.27.113:8085/en/");
+        driver.manage().window().maximize();
+
+    }
+
+    public void LoginExistingUser(String EmailId, String PassWord){
+        //Function to log into the existing user account
+        driver.findElement(By.xpath("//*[@id=\"_desktop_user_info\"]/div/a")).click();
+        driver.findElement(By.name("email")).sendKeys(EmailId);
+        driver.findElement(By.name("password")).sendKeys(PassWord);
+        driver.findElement(By.id("submit-login")).submit();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        //Close the Chrome webpage after performing each test case
+        driver.quit();
+    }
+
+}
