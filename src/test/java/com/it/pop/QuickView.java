c871bdb0f0b36df7fc584a443c948592a530a2d8
ShilpaK08 <82091226+ShilpaK08@users.noreply.github.com>
2021-04-20 12:10:48 +0200

    Delete src/test/java/com/it directory

--- src/test/java/com/it/pop/QuickView.java
+++
@@ -1,9 +0,0 @@
-package com.it.pop;
-
-import org.openqa.selenium.By;
-
-public class QuickView extends BaseClass{
-    public void clickView(String view) {
-        driver1.findElement(By.xpath(view)).submit();
-    }
-}
