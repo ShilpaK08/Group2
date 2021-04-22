d7cde5c9dd36395558733e088c37d66bdd569cff
ShilpaK08 <sonykulkarni.8@gmail.com>
2021-04-22 14:18:07 +0200

    editing ArtPage

--- FinalProject/src/test/java/com/it/ArtPage.java
+++ FinalProject/src/test/java/com/it/ArtPage.java
@@ -2,13 +2,19 @@ package com.it;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 public class ArtPage extends BaseClass{
 
-        public static String sixtyninty = "//*[@id=\"facet_12124\"]/li[2]/label/a";
+       public static String sixtyninty = "/html[1]/body[1]/main[1]/section[1]/div[1]/div[1]/div[2]/div[2]/section[4]/ul[1]/li[2]/label[1]/a[1]";
+       public static String eightyOneTwenty = "//body/main[1]/section[1]/div[1]/div[1]/div[2]/div[2]/section[2]/ul[1]/li[3]/label[1]/a[1]";
+       public static String fortysixty = "/html[1]/body[1]/main[1]/section[1]/div[1]/div[1]/div[2]/div[2]/section[4]/ul[1]/li[1]/label[1]/a[1]";
+       public static String sort = "//body/main[1]/section[1]/div[1]/div[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/button[1]";
 
-       public void OpenArtTab() {    driver.findElement(By.xpath("//*[@id=\"_desktop_top_menu\"]")).click();    }
+        public void OpenArtTab() {    driver.findElement(By.xpath("//a[contains(text(),'Art')]")).click();    }
 
         public void Composition() throws InterruptedException {
             //driver.get("http://40.76.27.113:8085/en/9-art");
@@ -25,17 +31,17 @@ public class ArtPage extends BaseClass{
 
         public void Dimensions(String Size) throws InterruptedException {
             driver.get("http://40.76.27.113:8085/en/9-art");
-            //Thread.sleep(1000);
             js.executeScript("window.scrollBy(0,150)");
-            driver.findElement(By.xpath(Size)).click();
             Thread.sleep(1000);
+            driver.findElement(By.xpath(sixtyninty)).click();
+
         }
 
-        public void DoubleFilter() throws InterruptedException {
-            driver.get("http://40.76.27.113:8085/en/9-art");
-            driver.findElement(By.partialLinkText("Graphic Corn")).click();
+        public void DoubleFilter(String size, String size1) throws InterruptedException {
+            OpenArtTab();
+            driver.findElement(By.xpath(fortysixty)).click();
             Thread.sleep(1000);
-            driver.findElement(By.partialLinkText("Matt pap")).click();
+            driver.findElement(By.xpath(eightyOneTwenty)).click();
             Thread.sleep(1000);
 
         }
@@ -43,11 +49,15 @@ public class ArtPage extends BaseClass{
         public void QuickView() throws InterruptedException {
             WebElement E1;
             driver.get("http://40.76.27.113:8085/en/9-art");
-            js.executeScript("window.scrollBy(0,350)");
-            E1 = driver.findElement(By.id("category-3"));
+            js.executeScript("window.scrollBy(0,450)");
+            E1 = driver.findElement(By.cssSelector("body.lang-en.country-se.currency-sek.layout-left-column.page-category.tax-display-enabled.category-id-9.category-art.category-id-parent-2.category-depth-level-2:nth-child(2) div.container div.left-column.col-xs-12.col-sm-8.col-md-9 div.products.row article.product-miniature.js-product-miniature:nth-child(5) div.thumbnail-container a.thumbnail.product-thumbnail > img:nth-child(1)"));
+            Actions action = new Actions(driver);
+            action.moveToElement(E1).perform();
             Thread.sleep(1000);
             //Bear poster
             driver.findElement(By.xpath("//*[@id=\"js-product-list\"]/div[1]/article[5]/div/div[2]/a")).click();
+            Thread.sleep(2000);
+
         }
 
 
@@ -59,22 +69,39 @@ public class ArtPage extends BaseClass{
         }
 
         public void Description() throws InterruptedException {
-            driver.get("http://40.76.27.113:8085/en/9-art");
+            OpenArtTab();
             Thread.sleep(1000);
-            js.executeScript("window.scrollBy(0,150)");
+            js.executeScript("window.scrollBy(0,200)");
             //Mountain fox frame
-            driver.findElement(By.xpath("//*[@id=\"js-product-list\"]/div[1]/article[4]/div/a/img")).click();
+            driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/div[2]/section[1]/section[1]/div[3]/div[1]/div[1]/article[4]/div[1]/a[1]/img[1]")).click();
             Thread.sleep(1000);
-            js.executeScript("window.scrollBy(0,250)");
+            js.executeScript("window.scrollBy(0,300)");
 
         }
 
-        public void Facebook() {
+    public void sortByDropDown(String Sortby, Integer index)
+    {
+        WebElement E2;
+        OpenArtTab();
+        js.executeScript("window.scrollBy(0,150)");
+        driver.findElement(By.xpath(sort)).click();
+        Actions keyDown = new Actions(driver);
+        keyDown.sendKeys(Keys.chord(Keys.DOWN, Keys.DOWN)).perform();
+        E2 = driver.findElement(By.xpath("//a[contains(text(),'Price, low to high')]"));
+        Actions action = new Actions(driver);
+        action.moveToElement(E2).perform();
+        action.click(E2).perform();
+        js.executeScript("window.scrollBy(0,250)");
+
+    }
+
+    public void Facebook() throws InterruptedException {
             driver.get("http://40.76.27.113:8085/en/9-art");
             js.executeScript("window.scrollBy(0,150)");
             driver.findElement(By.xpath("/html[1]/body[1]/main[1]/section[1]/div[1]/div[2]/section[1]/section[1]/div[3]/div[1]/div[1]/article[2]/div[1]/a[1]/img[1]")).click();
             driver.findElement(By.xpath("//a[contains(text(),'Share')]")).click();
-
+            Thread.sleep(1000);
+            driver.switchTo().window("Facebook");
         }
 
         public void Twitter() {
@@ -92,5 +119,9 @@ public class ArtPage extends BaseClass{
             driver.findElement(By.xpath("//a[contains(text(),'Pinterest')]")).click();
 
         }
+        public void title() {
 
+            driver.get("https://www.facebook.com/login.php?skip_api_login=1&api_key=966242223397117&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fsharer.php%3Fu%3Dhttp%253A%252F%252F40.76.27.113%253A8085%252Fen%252Fart%252F4-the-adventure-begins-framed-poster.html&cancel_url=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Fclose_window%2F%3Fapp_id%3D966242223397117%26connect%3D0%23_%3D_&display=popup&locale=en_GB");
+
+        }
 }
--- FinalProject/src/test/java/com/it/TestArtPage.java
+++ FinalProject/src/test/java/com/it/TestArtPage.java
@@ -37,14 +37,17 @@ public class TestArtPage extends ArtPage {
     @Test
     public void TestDimensions() throws InterruptedException {
         Dimensions(sixtyninty);
-        assertTrue(driver.findElement(By.xpath("//*[@id=\"facet_72656\"]/li[2]/label/span/span/i")).isEnabled());
-        String curText = driver.findElement(By.xpath("//*[@id=\"js-active-search-filters\"]/p")).getText();
+        Thread.sleep(1000);
+        assertTrue(driver.findElement(By.xpath("/html[1]/body[1]/main[1]/section[1]/div[1]/div[1]/div[2]/div[2]/section[2]/ul[1]/li[2]/label[1]/span[1]/span[1]/i[1]")).isEnabled());
+        String curText = driver.findElement(By.xpath("//p[contains(text(),'Active filters')]")).getText();
         Assertions.assertEquals(curText, "Active filters");
     }
 
     @Test
     public void TestDoubleFilter() throws InterruptedException {
-        DoubleFilter();
+        DoubleFilter(fortysixty,eightyOneTwenty);
+        assertTrue(driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/div[1]/div[2]/div[2]/section[2]/ul[1]/li[1]/label[1]/span[1]/span[1]/i[1]")).isEnabled());
+        assertTrue(driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/div[1]/div[2]/div[2]/section[2]/ul[1]/li[3]/label[1]/span[1]/span[1]/i[1]")).isEnabled());
         String curText = driver.findElement(By.xpath("//*[@id=\"js-active-search-filters\"]/p")).getText();
         String expText = "Active filters";
         assertEquals(curText,expText);
@@ -54,19 +57,51 @@ public class TestArtPage extends ArtPage {
     @Test
     public void TestDescription() throws InterruptedException {
         Description();
-        String curText = driver.findElement(By.xpath("//*[@id=\"description\"]/div/p/span")).getText();
+        String curText = driver.findElement(By.xpath("//span[contains(text(),'You have a custom printing creative project? The v')]")).getText();
         //String curText = driver.findElement(By.partialLinkText("You have a custom printing creative project?")).getText();
         String expText = "You have a custom printing creative project? The vector graphic Mountain fox illustration can be used for printing purpose on any support, without size limitation.";
         assertEquals(curText, expText);
 
     }
 
+    @Test
+    public void TestSortByDropDown() throws InterruptedException {
+        sortByDropDown(sort,3 );
+        assertTrue(driver.findElement(By.xpath(sort)).isEnabled());
+        //String curURL = driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/div[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/button[1]")).getText();
+        //String expURL = "Price, low to high";
+
+        String curUrl = driver.getCurrentUrl();
+        //String expURL = "http://40.76.27.113:8085/en/9-art?order=product.price.asc";
+        Assertions.assertEquals("http://40.76.27.113:8085/en/9-art?order=product.price.asc",curUrl);
+
+    }
+
     @Test //not working
     public void TestQuickView() throws InterruptedException {
         QuickView();
-        String curText = driver.findElement(By.xpath("//*[@id=\"quickview-modal-13-0\"]/div/div/div[2]/div/div[2]/h1")).getText();
+        //String curText = driver.findElement(By.xpath("//h1[contains(text(),'Brown bear - Vector graphics')]")).getText();
+        String curText = driver.switchTo().alert().getText();
         String expText = "BROWN BEAR - VECTOR GRAPHICS";
-        assertEquals(curText,expText);
+        assertEquals(expText,curText);
+
+    }
+    @Test
+    public void TestFacebook() throws InterruptedException {
+        Facebook();
+
+        String expUrl = "https://www.facebook.com/sharer.php?u=http%3A%2F%2F40.76.27.113%3A8085%2Fen%2Fart%2F4-the-adventure-begins-framed-poster.html";
+        String curUrl = driver.getCurrentUrl();
+        Assertions.assertEquals(expUrl,curUrl);
+
+    }
+
+    @Test
+    public void testtitle() {
+        title();
+        String s = driver.getTitle();
+        Assertions.assertEquals("sdsds", s);
+
 
     }
 
