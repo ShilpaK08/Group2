ed6a0e3ff31a2ff93c9342a61a93e12ba37e8380
akshatha00 <aksh.bms@gmail.com>
2021-04-22 15:58:48 +0200

    Added stationary file

--- FinalProject/src/test/java/com/it/Stationary.java
+++ FinalProject/src/test/java/com/it/Stationary.java
@@ -1,6 +1,6 @@
 package com.it;
 
-
+import com.it.BaseClass.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -8,15 +8,18 @@ import org.openqa.selenium.interactions.Actions;
 
     public class Stationary extends BaseClass
     {
+            public static String ChooseSortBy = "/html/body/main/section/div/div[2]/section/section/div[1]/div/div[2]/div/div[1]/div/a[2]";
+            public static String Ruled = "/html/body/main/section/div/div[1]/div[2]/div[2]/section[2]/ul/li[1]/label/a";
+
         public void PaperType(String papertype)
         {
             {
-                driver.findElement(By.xpath(papertype)).click();
+                driver.findElement(By.xpath(Ruled)).click();
             }
 
         }
-        public void ChooseSortBy(String dropdown, int indexNo) {
-            WebElement E = driver.findElement(By.xpath(dropdown));
+        public void SelectSortby(String ChooseSortBy, int indexNo) {
+            WebElement E = driver.findElement(By.xpath(ChooseSortBy));
             E.click();
             Actions keyDown = new Actions(driver);
             keyDown.sendKeys(new CharSequence[]{Keys.chord(new CharSequence[]{Keys.DOWN, Keys.DOWN})}).perform();
