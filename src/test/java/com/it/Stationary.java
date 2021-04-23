4a17f5840249b9b857d0b29db8e4b7c96252c4f7
akshatha00 <aksh.bms@gmail.com>
2021-04-24 00:30:02 +0200

    Update stationary

--- src/test/java/com/it/Stationary.java
+++ src/test/java/com/it/Stationary.java
@@ -3,59 +3,58 @@ package com.it;
 import com.it.BaseClass.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-    public class Stationary extends BaseClass
-    {
-        public String SortbyRelevance = "/html/body/main/section/div/div[2]/section/section/div[1]/div/div[2]/div/div[1]/div/a[2]";
+    public class Stationary extends BaseClass {
+
         public String Ruled = "/html/body/main/section/div/div[1]/div[2]/div[2]/section[2]/ul/li[1]/label/a";
         public String MountainFoxNotebook = "/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[1]/div/div[1]/h2/a";
-        public String Nextpage = "/html/body/main/section/div/div[1]/div[2]/div[2]/section[5]/p";
+        public String SortbyRelevance = "/html/body/main/section/div/div[2]/section/section/div[1]/div/div[2]/div/div[1]/button";
+        public String Accessories = "/html/body/main/header/div[2]/div/div[1]/div[2]/div[1]/ul/li[2]/a";
 
-        public void openStatio(String Statio)
+
+        public void openStationary(String stationary)
         {
 
             Actions action = new Actions(driver);
-            WebElement we = driver.findElement(By.xpath(Statio));
-            action.moveToElement(we).moveToElement(driver.findElement(By.xpath("/html/body/main/header/div[2]/div/div[1]/div[2]/div[1]/ul/li[2]/div/ul/li[1]/a"))).click().build().perform();
+            WebElement E = driver.findElement(By.xpath(stationary));
+            action.moveToElement(E).moveToElement(driver.findElement(By.xpath("//*[@id=\"category-7\"]/a"))).click().build().perform();
         }
 
         public void PaperType(String papertype)
         {
-            {
-                driver.get("http://40.76.27.113:8085/en/7-stationery");
-                WebElement E = driver.findElement(By.xpath(Ruled));
-                driver.findElement(By.xpath(Ruled)).click();
-            }
+            driver.findElement(By.xpath(papertype)).click();
 
         }
 
-        public void ChooseRelevance(String SortbyRelevance, Integer index)
-    {
+        public void selectDropDown(String SortbyRelevance, int indexNo) {
 
-        WebElement E= driver.findElement(By.xpath(SortbyRelevance));
-        E.click();
-        Actions keyDown = new Actions(driver);
-        keyDown.sendKeys(Keys.chord(Keys.DOWN, Keys.DOWN)).perform();
-    }
+            WebElement selectMyElement = driver.findElement(By.xpath(SortbyRelevance));
+            selectMyElement.click();
 
-        public void OneItem(String item)
+            Actions keyDown = new Actions(driver);
+            keyDown.sendKeys(Keys.chord(Keys.DOWN, Keys.DOWN)).perform();
+        }
+
+        public void Item(String item)
         {
-
             driver.findElement(By.xpath(item)).click();
+
         }
 
-        public void clickView(String view)
+       /* public void clickView(String view)
         {
-
             Actions action = new Actions(driver);
             WebElement E = driver.findElement(By.xpath(view));
-            action.moveToElement(E).moveToElement(driver.findElement(By.xpath("/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[1]/div/div[2]/a"))).click().build().perform();
+            action.moveToElement(E).moveToElement(driver.findElement(By.xpath("/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[3]/div/div[2]/a"))).click().build().perform();
 
-        }
-
+        }*/
     }
+
+
+
 
 
 
