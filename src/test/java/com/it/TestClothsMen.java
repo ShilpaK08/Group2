f2b3f3f53c11adbb7ac5b8bcb152f4a839e7aec3
Mathu <cjmathu@gmail.com>
2021-05-01 12:06:13 +0200

    Clothesmen updated

--- src/test/java/com/it/TestClothsMen.java
+++ src/test/java/com/it/TestClothsMen.java
@@ -13,31 +13,19 @@ import javax.swing.*;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestClothsMen extends ClothsMen {
     //TC for Cloths Men from Home page
     @Test
     public void ClickonCloths() throws InterruptedException {
-        // LoginExistingUser("testacct@gmail.com","Test@1234");
+        //LoginExistingUser("testacct@gmail.com","Test@1234");
         SelectMen();
-        WebElement E3 = driver.findElement(By.xpath("//*[@id=\"wrapper\"]/div/nav/ol"));
-        String S0 = E3.getText();
-        Assertions.assertEquals("Home Clothes Men", S0);
+        String currenturl = driver.getCurrentUrl();
         Thread.sleep(1000);
-        WebElement E4 = driver.findElement(By.xpath("//*[@id=\"left-column\"]/div[1]/ul/li[1]/a"));
-        String S1 = E4.getText();
-        Assertions.assertEquals("MEN", S1);
-        Thread.sleep(1000);
-        WebElement E5 = driver.findElement(By.xpath("//*[@id=\"search_filters\"]/p"));
-        String S2 = E5.getText();
-        Assertions.assertEquals("FILTER BY", S2);
-        Thread.sleep(1000);
-        WebElement E6 = driver.findElement(By.xpath("//*[@id=\"js-product-list-header\"]/div/h1"));
-        String S3 = E6.getText();
-        Assertions.assertEquals("MEN", S3);
-        Thread.sleep(1000);
-
+        String url = "http://40.76.27.113:8085/en/4-men";
+        assertEquals(url, currenturl);
     }
 
     //Filter by size option
@@ -58,7 +46,6 @@ public class TestClothsMen extends ClothsMen {
         //LoginExistingUser("testacct@gmail.com","Test@1234");
         SelectMen();
         ChooseColor(Black);
-        Thread.sleep(500);
         assertTrue(driver.findElement(By.xpath(ColorResult)).isEnabled());
     }
 
@@ -89,60 +76,15 @@ public class TestClothsMen extends ClothsMen {
     //Click on product image and verify the page
     @Test
     public void ClickonProductImage() throws InterruptedException {
-        //LoginExistingUser("testacct@gmail.com", "Test@1234");
+        LoginExistingUser("testacct@gmail.com", "Test@1234");
         SelectMen();
         driver.findElement(By.xpath
                 ("//body/main[1]/section[1]/div[1]/div[2]/section[1]/section[1]/div[3]/div[1]/div[1]/article[1]/div[1]/a[1]")).click();
-        //Breadcrumb verification
-        WebElement E10 = driver.findElement(By.xpath("//*[@id=\"wrapper\"]/div/nav/ol"));
-        String S5 = E10.getText();
-        Assertions.assertEquals("Home Clothes Men Hummingbird printed t-shirt", S5);
-        //Page header verification
-        String S6 = driver.findElement(By.xpath("//h1[contains(text(),'Hummingbird printed t-shirt')]")).getText();
-        System.out.println(S6);
-        Assertions.assertEquals("HUMMINGBIRD PRINTED T-SHIRT", S6);
-        //Size dropdown verification
-        WebElement E11 = driver.findElement(By.xpath("//select[@id='group_1']"));
-        E11.click();
-        Thread.sleep(500);
-        if(E11.isEnabled() && E11.isDisplayed())
-        {
-            System.out.println("Dropdown is enabled and visible");
-        }
-        else {
-            System.out.println("Dropdown is not visible");
-        }
-        Thread.sleep(500);
-        //Quantity dropdown verification
-        WebElement E12 = driver.findElement(By.xpath("//span[contains(text(),'Quantity')]"));
-        String S7 = E12.getText();
-        Assertions.assertEquals("Quantity", S7);
-        Thread.sleep(500);
-        WebElement E13 = driver.findElement(By.xpath("//input[@id='quantity_wanted']"));
-        if(E13.isEnabled())
-        {
-            System.out.println("Dropdown is enabled");
-        }
-        else
-        {
-            System.out.println("Drop down is not enabled");
-        }
-        Thread.sleep(500);
-        driver.findElement(By.xpath
-                ("//body/main[1]/section[1]/div[1]/div[1]/section[1]/div[1]/div[2]/div[2]/div[2]" +
-                        "/form[1]/div[2]/div[1]/div[1]/div[1]/span[3]/button[1]/i[1]")).click();
-        System.out.println("The quantity is added");
-        Thread.sleep(500);
-        driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/div[1]/section[1]/div[1]/div[2]" +
-                "/div[2]/div[2]/form[1]/div[2]/div[1]/div[2]/button[1]")).click();
-        //Add to cart page verification
-        driver.switchTo().activeElement();
-        Thread.sleep(500);
-        //WebElement E15 = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/h4[1]"));
-        //String S8 = E15.getText();
-        //Assertions.assertEquals("Product successfully added to your shopping cart", S8);
-        driver.findElement(By.xpath("//body/div[@id='blockcart-modal']/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/a[1]")).click();
-        Thread.sleep(500);
+        String curl = driver.getCurrentUrl();
+        Thread.sleep(1000);
+        String URL = "http://40.76.27.113:8085/en/men/1-1-hummingbird-printed-t-shirt.html#/1-size-s/8-color-white";
+        assertEquals(URL, curl);
+
     }
 
     //Quickview functionality in clothsMen page
@@ -155,6 +97,7 @@ public class TestClothsMen extends ClothsMen {
         Actions action1 = new Actions(driver);
         action1.moveToElement(E16).perform();
         driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/div[2]/section[1]/section[1]/div[3]/div[1]/div[1]/article[1]/div[1]/div[2]/a[1]")).click();
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//span[contains(text(),'×')]")).click();
     }
 
@@ -170,15 +113,14 @@ public class TestClothsMen extends ClothsMen {
         driver.findElement(By.xpath
                 ("//body/main[1]/section[1]/div[1]/div[1]/section[1]/div[1]/div[2]/div[2]/div[2]/form[1]/div[2]/div[1]/div[2]/button[1]")).click();
         driver.switchTo().activeElement();
-        Thread.sleep(500);
+        Thread.sleep(1000);
         //Click on continue shopping
         driver.findElement(By.xpath("//button[contains(text(),'Continue shopping')]")).click();
-        Thread.sleep(500);
+        Thread.sleep(1000);
         //Home  Clothes  Men  Hummingbird printed t-shirt page header verification
         WebElement E17 = driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/nav[1]/ol[1]"));
         String S8 = E17.getText();
         Assertions.assertEquals("Home Clothes Men Hummingbird printed t-shirt", S8);
-        Thread.sleep(500);
     }
 
     //ZoomIn functionality
