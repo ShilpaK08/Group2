9f1765ea9284d93ece1ee25dbcedd8adb4211caa
Mathu <cjmathu@gmail.com>
2021-05-01 10:24:51 +0200

    Clothsmen updated

--- src/test/java/com/it/TestClothsMen.java
+++ src/test/java/com/it/TestClothsMen.java
@@ -17,9 +17,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestClothsMen extends ClothsMen {
     //TC for Cloths Men from Home page
-  /*  @Test
+    @Test
     public void ClickonCloths() throws InterruptedException {
-       // LoginExistingUser("testacct@gmail.com","Test@1234");
+        // LoginExistingUser("testacct@gmail.com","Test@1234");
         SelectMen();
         WebElement E3 = driver.findElement(By.xpath("//*[@id=\"wrapper\"]/div/nav/ol"));
         String S0 = E3.getText();
@@ -68,24 +68,22 @@ public class TestClothsMen extends ClothsMen {
 
     //Product image verification
     @Test
-    public void ProductImage() throws InterruptedException
-    {
-       // LoginExistingUser("testacct@gmail.com","Test@1234");
+    public void ProductImage() throws InterruptedException {
+        // LoginExistingUser("testacct@gmail.com","Test@1234");
         SelectMen();
         WebElement E9 = driver.findElement(By.xpath("//*[@id=\"js-product-list\"]/div[1]/article/div/a/img"));
         js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView();", E9 );
+        js.executeScript("arguments[0].scrollIntoView();", E9);
         Actions action1 = new Actions(driver);
         action1.moveToElement(E9).perform();
-        Thread.sleep(2000);
+        Thread.sleep(500);
         System.out.println("The quickview is displayed");
         driver.findElement(By.xpath("//*[@id=\"js-product-list\"]/div[1]/article/div/div[2]/a")).click();
     }
 
     //Click on product image and verify the page
     @Test
-    public void ClickonProductImage() throws InterruptedException
-    {
+    public void ClickonProductImage() throws InterruptedException {
         //LoginExistingUser("testacct@gmail.com", "Test@1234");
         SelectMen();
         driver.findElement(By.xpath
@@ -101,11 +99,9 @@ public class TestClothsMen extends ClothsMen {
         //Size dropdown verification
         WebElement E11 = driver.findElement(By.xpath("//select[@id='group_1']"));
         E11.click();
-        if(E11.isEnabled() && E11.isDisplayed())
-        {
+        if (E11.isEnabled() && E11.isDisplayed()) {
             System.out.println("Dropdown is enabled and visible");
-        }
-        else {
+        } else {
             System.out.println("Dropdown is not visible");
         }
         //Quantity dropdown verification
@@ -113,12 +109,9 @@ public class TestClothsMen extends ClothsMen {
         String S7 = E12.getText();
         Assertions.assertEquals("Quantity", S7);
         WebElement E13 = driver.findElement(By.xpath("//input[@id='quantity_wanted']"));
-        if(E13.isEnabled())
-        {
+        if (E13.isEnabled()) {
             System.out.println("Dropdown is enabled");
-        }
-        else
-        {
+        } else {
             System.out.println("Drop down is not enabled");
         }
         driver.findElement(By.xpath
@@ -129,22 +122,21 @@ public class TestClothsMen extends ClothsMen {
                 "/div[2]/div[2]/form[1]/div[2]/div[1]/div[2]/button[1]")).click();
         //Add to cart page verification
         driver.switchTo().activeElement();
-        Thread.sleep(5000);
+        Thread.sleep(500);
         //WebElement E15 = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/h4[1]"));
         //String S8 = E15.getText();
         //Assertions.assertEquals("Product successfully added to your shopping cart", S8);
         driver.findElement(By.xpath("//body/div[@id='blockcart-modal']/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/a[1]")).click();
-        Thread.sleep(2000);
+        Thread.sleep(500);
     }
 
     //Quickview functionality in clothsMen page
     @Test
-    public void QuickviewMan() throws InterruptedException
-    {
+    public void QuickviewMan() throws InterruptedException {
         //LoginExistingUser("testacct@gmail.com", "Test@1234");
         SelectMen();
         WebElement E16 = driver.findElement(By.xpath("//*[@id=\"js-product-list\"]/div[1]/article/div/a/img"));
-        js.executeScript("arguments[0].scrollIntoView();", E16 );
+        js.executeScript("arguments[0].scrollIntoView();", E16);
         Actions action1 = new Actions(driver);
         action1.moveToElement(E16).perform();
         driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/div[2]/section[1]/section[1]/div[3]/div[1]/div[1]/article[1]/div[1]/div[2]/a[1]")).click();
@@ -153,8 +145,7 @@ public class TestClothsMen extends ClothsMen {
 
     //Continue Shopping in add to cart page
     @Test
-    public void ContinueShopping() throws InterruptedException
-    {
+    public void ContinueShopping() throws InterruptedException {
         LoginExistingUser("testacct@gmail.com", "Test@1234");
         SelectMen();
         //ProductImage clicking
@@ -164,10 +155,10 @@ public class TestClothsMen extends ClothsMen {
         driver.findElement(By.xpath
                 ("//body/main[1]/section[1]/div[1]/div[1]/section[1]/div[1]/div[2]/div[2]/div[2]/form[1]/div[2]/div[1]/div[2]/button[1]")).click();
         driver.switchTo().activeElement();
-        Thread.sleep(1000);
+        Thread.sleep(500);
         //Click on continue shopping
         driver.findElement(By.xpath("//button[contains(text(),'Continue shopping')]")).click();
-        Thread.sleep(1000);
+        Thread.sleep(500);
         //Home  Clothes  Men  Hummingbird printed t-shirt page header verification
         WebElement E17 = driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/nav[1]/ol[1]"));
         String S8 = E17.getText();
@@ -175,7 +166,7 @@ public class TestClothsMen extends ClothsMen {
     }
 
     //ZoomIn functionality
-    @Test
+    /*@Test
     public void ZoomIn() throws InterruptedException
     {
         LoginExistingUser("testacct@gmail.com", "Test@1234");
@@ -187,14 +178,17 @@ public class TestClothsMen extends ClothsMen {
         Actions action = new Actions(driver);
         WebElement E18 = driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/div[1]/section[1]/div[1]/div[1]/section[1]/div[1]/div[1]/div[1]"));
         action.moveToElement(E18).build().perform();
-        Thread.sleep(2000);
+        Thread.sleep(500);
         WebElement E19 = driver.findElement(By.xpath("//i[contains(text(),'\uE8FF')]"));
         E19.click();
         //WebDriverWait wait = new WebDriverWait(driver, 10);
-        Thread.sleep(2000);
-        if(E19.isDisplayed())
-            System.out.println("The zoom is visible");
-        else
-            System.err.println("The zoom isn't visible!!");
+        Thread.sleep(500);
+        Actions act = new Actions(driver);
+        WebElement E20 = driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/div[1]/section[1]/div[2]/div[1]/div[1]/div[1]/figure[1]/img[1]"));
+        act.moveToElement(E20).perform();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//body/main[1]/section[1]/div[1]/div[1]/section[1]/div[1]")).click();
+        Thread.sleep(500);
+        //driver.switchTo().frame(driver.findElement(By.xpath("/html[1]/body[1]/main[1]/section[1]"))).getWindowHandle();
     }*/
 }
