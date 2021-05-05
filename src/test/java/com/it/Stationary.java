144f017f4114afb36fa82db672480d12d30bad4a
akshatha00 <aksh.bms@gmail.com>
2021-05-05 11:14:04 +0200

    Commet TC

--- src/test/java/com/it/Stationary.java
+++ src/test/java/com/it/Stationary.java
@@ -20,15 +20,12 @@ import org.openqa.selenium.interactions.Actions;
 
     public class Stationary extends BaseClass {
 
-        public String Ruled = "/html/body/main/section/div/div[1]/div[2]/div[2]/section[2]/ul/li[1]/label/a";
+        public String papertype = "/html/body/main/section/div/div[1]/div[2]/div[2]/section[2]/ul/li[1]/label/a";
         public String MountainFoxNotebook = "/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[1]/div/div[1]/h2/a";
         public String SortbyRelevance = "/html/body/main/section/div/div[2]/section/section/div[1]/div/div[2]/div/div[1]/button";
         public String Accessories = "/html/body/main/header/div[2]/div/div[1]/div[2]/div[1]/ul/li[2]/a";
-        public String Newpage = "/html/body/div[1]/div/div/div[2]/div/div[1]/div[1]/div[1]/img";
-        public String Mountain = "/html/body/main/section/div/div/section/div[1]/div[2]/div[2]/div[4]/ul/li[1]/a";
-
-
-
+        public String view = "/html/body/div[1]/div/div/div[2]/div/div[1]/div[1]/div[1]/img";
+        public String description = "/html/body/main/section/div/div/section/div[1]/div[2]/div[2]/div[4]/ul/li[1]/a";
 
         public void openStationary(String stationary)
         {
@@ -58,19 +55,18 @@ import org.openqa.selenium.interactions.Actions;
 
         }
 
-        /*public void clickView(String view)
+        public void clickView(String view)
         {
             Actions action = new Actions(driver);
             WebElement E = driver.findElement(By.xpath(view));
             action.moveToElement(E).moveToElement(driver.findElement(By.xpath("/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[3]/div/div[2]/a"))).click().build().perform();
 
         }
-        public void Description() throws InterruptedException
+        public void Description(String description)
         {
             js.executeScript("window.scrollBy(0,250)");
-            Thread.sleep(1000);
-            driver.findElement(By.xpath(Mountain)).click();
-        }*/
+            driver.findElement(By.xpath(description)).click();
+        }
 
     }
 
--- src/test/java/com/it/TestStationary.java
+++ src/test/java/com/it/TestStationary.java
@@ -19,15 +19,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
     public class TestStationary  extends Stationary
     {
-        /*@Test
+        @Test
         public void testPaperType() throws InterruptedException
         {
             Thread.sleep(2000);
             openStationary(Accessories);
             Thread.sleep(2000);
-            PaperType(Ruled);
-            assertTrue(driver.findElement(By.xpath(Ruled)).isEnabled());
-        }*/
+            assertTrue(driver.findElement(By.xpath(papertype)).isEnabled());
+        }
 
         @Test
         public void testDropDown() throws InterruptedException
@@ -36,7 +35,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
             openStationary(Accessories);
             Thread.sleep(2000);
             selectDropDown(SortbyRelevance, 1);
-           // assertTrue(driver.findElement(By.xpath(SortbyRelevance)).isEnabled());
+            assertTrue(driver.findElement(By.xpath(SortbyRelevance)).isEnabled());
         }
 
 
@@ -47,19 +46,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
             openStationary(Accessories);
             Thread.sleep(2000);
             Item(MountainFoxNotebook);
-           // assertEquals(driver.getCurrentUrl(), "http://40.76.27.113:8085/en/stationery/16-28-mountain-fox-notebook.html#/22-paper_type-ruled");
+            assertEquals(driver.getCurrentUrl(), "http://40.76.27.113:8085/en/stationery/16-28-mountain-fox-notebook.html#/22-paper_type-ruled");
         }
 
 
-
-
-
-        /*@Test
-
-        public void testqView() throws InterruptedException
+        @Test
+        public void testView() throws InterruptedException
         {
             Thread.sleep(2000);
-            WebElement Element = driver.findElement(By.xpath(Newpage));
+            openStationary(Accessories);
+            WebElement Element = driver.findElement(By.xpath(view));
             js.executeScript("arguments[0].scrollIntoView();", Element);
             Thread.sleep(2000);
             clickView(MountainFoxNotebook);
@@ -69,8 +65,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
         @Test
         public void TestDescription() throws InterruptedException
         {
-            Description();
+            Description("mountain");
             Thread.sleep(1000);
-            Assertions.assertTrue(driver.findElement(By.xpath(Mountain)).isDisplayed());
-        }*/
+            Assertions.assertTrue(driver.findElement(By.xpath(description)).isDisplayed());
+        }
     }
