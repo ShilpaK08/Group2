f7dd43970f50a3b76fc656802247380c5d1cc6f4
akshatha00 <aksh.bms@gmail.com>
2021-05-07 10:39:09 +0200

    Swedish TC

---
+++ src/test/java/com/it/SwedishFooter.java
@@ -0,0 +1,72 @@
+package com.it;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class SwedishFooter
+{
+    public static String Mailaoss = "/html/body/main/footer/div[2]/div/div[1]/div[3]/div[1]/a";
+    public static String Rea = "/html/body/main/footer/div[2]/div/div[1]/div[1]/div/div[1]/ul/li[1]/a";
+    public static String Delivery = "/html/body/main/footer/div[2]/div/div[1]/div[1]/div/div[2]/ul/li[1]/a";
+    public static String LegalNotice = "/html/body/main/footer/div[2]/div/div[1]/div[1]/div/div[2]/ul/li[2]/a";
+
+    private static JavascriptExecutor js;
+    public static WebDriver driver;
+
+    public static void SetUp(){
+        System.setProperty("webdriver.chrome.driver","C:\\WebDriver\\bin\\chromedriver.exe");
+        driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+        driver.get("http://40.76.27.113:8085/sv/");
+        driver.manage().window().maximize();
+
+    }
+
+    public void LoginExistingUser(String EmailId, String PassWord){
+        //Function to log into the existing user account
+        driver.findElement(By.xpath("//*[@id=\"_desktop_user_info\"]/div/a")).click();
+        driver.findElement(By.name("email")).sendKeys(EmailId);
+        driver.findElement(By.name("password")).sendKeys(PassWord);
+        driver.findElement(By.id("submit-login")).submit();
+    }
+
+    public void Products() throws InterruptedException {
+        driver.get("http://40.76.27.113:8085/sv/");
+        js.executeScript("window.scrollBy(0,150)");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(Rea)).click();
+    }
+
+    public void ourCompany() throws InterruptedException {
+        driver.get("http://40.76.27.113:8085/sv/");
+        js.executeScript("window.scrollBy(0,150)");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(Delivery)).click();
+        driver.findElement(By.xpath(LegalNotice)).click();
+    }
+
+    public void BUTIKSINFORMATION() throws InterruptedException {
+        driver.get("http://40.76.27.113:8085/sv/");
+        js.executeScript("window.scrollBy(0,150)");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(Mailaoss)).click();
+    }
+
+    public static void tearDown(){
+        // Close the Chrome webpage after performing each test case
+        driver.quit();
+    }
+
+
+}
---
+++ src/test/java/com/it/TestsFooter.java
@@ -0,0 +1,39 @@
+package com.it;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class TestsFooter extends SwedishFooter
+{
+    @Test
+    public void testProducts() throws InterruptedException
+    {
+
+        Thread.sleep(2000);
+        Assertions.assertTrue(driver.findElement(By.xpath(Rea)).isDisplayed());
+        Thread.sleep(2000);
+
+    }
+
+    @Test
+    public void testOurCompany() throws InterruptedException
+    {
+        ourCompany();
+        Thread.sleep(2000);
+        Assertions.assertTrue(driver.findElement(By.xpath(Delivery)).isDisplayed());
+        Thread.sleep(2000);
+        Assertions.assertTrue(driver.findElement(By.xpath(LegalNotice)).isDisplayed());
+        driver.findElement(By.xpath("/html/body/main/footer/div[2]/div/div[1]/div[1]/div/div[1]/ul/li[2]/a")).sendKeys("note");
+        driver.findElement(By.xpath("/html/body/main/section/div/div/section/section/p[2]/a[1]")).click();
+
+    }
+    @Test
+    public void testBUTIKSINFORMATION() throws InterruptedException
+    {
+        BUTIKSINFORMATION();
+        Thread.sleep(2000);
+        Assertions.assertTrue(driver.findElement(By.xpath(Mailaoss)).isDisplayed());
+    }
+
+}
