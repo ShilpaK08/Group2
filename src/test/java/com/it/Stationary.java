765baf18c2276d342cb1ebb79208c924bd851c3f
akshatha00 <aksh.bms@gmail.com>
2021-05-07 22:10:21 +0200

    updated stationary and footer

--- src/test/java/com/it/Stationary.java
+++ src/test/java/com/it/Stationary.java
@@ -1,78 +1,60 @@
 package com.it;
 
-import com.it.BaseClass.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
-    public class Stationary extends BaseClass {
+public class Stationary extends BaseClass {
 
-        public String Ruled = "/html/body/main/section/div/div[1]/div[2]/div[2]/section[2]/ul/li[1]/label/a";
-        public String MountainFoxNotebook = "/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[1]/div/div[1]/h2/a";
-        public String SortbyRelevance = "/html/body/main/section/div/div[2]/section/section/div[1]/div/div[2]/div/div[1]/button";
-        public String Accessories = "/html/body/main/header/div[2]/div/div[1]/div[2]/div[1]/ul/li[2]/a";
-        public String Newpage = "/html/body/div[1]/div/div/div[2]/div/div[1]/div[1]/div[1]/img";
-        public String Mountain = "/html/body/main/section/div/div/section/div[1]/div[2]/div[2]/div[4]/ul/li[1]/a";
+    public String paperType = "/html/body/main/section/div/div[1]/div[2]/div[2]/section[2]/ul/li[1]/label/a";
+    public String MountainFoxNotebook = "/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[1]/div/div[1]/h2/a";
+    public String SortByRelevance = "/html/body/main/section/div/div[2]/section/section/div[1]/div/div[2]/div/div[1]/button";
+    public String Accessories = "/html/body/main/header/div[2]/div/div[1]/div[2]/div[1]/ul/li[2]/a";
+    public String view = "/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[1]/div/div[2]/a";
+    public String mountain = "/html/body/main/section/div/div/section/div[1]/div[2]/div[2]/div[4]/ul/li[1]/a";
+    public String twitter = "/html/body/main/section/div/div/section/div[1]/div[2]/div[2]/div[2]/form/div[3]/div/ul/li[2]/a";
 
+    public void openStationary(String stationaryXpath)
+    {
 
+        Actions action = new Actions(driver);
+        WebElement E = driver.findElement(By.xpath(stationaryXpath));
+        action.moveToElement(E).moveToElement(driver.findElement(By.xpath("//*[@id=\"category-7\"]/a"))).click().build().perform();
+    }
 
+    public void PaperType(String paperType) {
+        driver.findElement(By.xpath(paperType)).click();
+    }
 
-        public void openStationary(String stationary)
-        {
+    public void selectDropDown(String SortbyRelevance, int indexNo) {
 
-            Actions action = new Actions(driver);
-            WebElement E = driver.findElement(By.xpath(stationary));
-            action.moveToElement(E).moveToElement(driver.findElement(By.xpath("//*[@id=\"category-7\"]/a"))).click().build().perform();
-        }
+        WebElement selectMyElement = driver.findElement(By.xpath(SortbyRelevance));
+        selectMyElement.click();
+        Actions keyDown = new Actions(driver);
+        keyDown.sendKeys(Keys.chord(Keys.DOWN, Keys.DOWN)).perform();
+    }
 
-        public void PaperType(String papertype)
-        {
-            driver.findElement(By.xpath(papertype)).click();
-
-        }
-
-        public void selectDropDown(String SortbyRelevance, int indexNo) {
-
-            WebElement selectMyElement = driver.findElement(By.xpath(SortbyRelevance));
-            selectMyElement.click();
-            Actions keyDown = new Actions(driver);
-            keyDown.sendKeys(Keys.chord(Keys.DOWN, Keys.DOWN)).perform();
-        }
-
-        public void Item(String item)
-        {
-            driver.findElement(By.xpath(item)).click();
-
-        }
-
-        /*public void clickView(String view)
-        {
-            Actions action = new Actions(driver);
-            WebElement E = driver.findElement(By.xpath(view));
-            action.moveToElement(E).moveToElement(driver.findElement(By.xpath("/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[3]/div/div[2]/a"))).click().build().perform();
-
-        }
-        public void Description() throws InterruptedException
-        {
-            js.executeScript("window.scrollBy(0,250)");
-            Thread.sleep(1000);
-            driver.findElement(By.xpath(Mountain)).click();
-        }*/
+    public void item(String item) {
+        driver.findElement(By.xpath(item)).click();
 
     }
+
+    public void quickView(String view) {
+        Actions action = new Actions(driver);
+        WebElement E = driver.findElement(By.xpath(view));
+        action.moveToElement(E).moveToElement(driver.findElement(By.xpath("/html/body/main/section/div/div[2]/section/section/div[3]/div/div[1]/article[3]/div/div[2]/a"))).click().build().perform();
+    }
+    /*public void clickShare(String link)
+    {
+        driver.findElement(By.xpath(link)).click();
+    }*/
+
+    public void description(String mountain) {
+        js.executeScript("window.scrollBy(0,200)");
+        driver.findElement(By.xpath(mountain)).click();
+    }
+}
 
 
 
--- src/test/java/com/it/TestStationary.java
+++ src/test/java/com/it/TestStationary.java
@@ -1,76 +1,80 @@
 package com.it;
 
-import com.it.BaseClass.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+public class TestStationary extends Stationary
+{
 
-    public class TestStationary  extends Stationary
+    @Test
+    public void testPaperType() throws InterruptedException
     {
-        /*@Test
-        public void testPaperType() throws InterruptedException
-        {
-            Thread.sleep(2000);
-            openStationary(Accessories);
-            Thread.sleep(2000);
-            PaperType(Ruled);
-            assertTrue(driver.findElement(By.xpath(Ruled)).isEnabled());
-        }*/
-
-        @Test
-        public void testDropDown() throws InterruptedException
-        {
-            Thread.sleep(2000);
-            openStationary(Accessories);
-            Thread.sleep(2000);
-            selectDropDown(SortbyRelevance, 1);
-           // assertTrue(driver.findElement(By.xpath(SortbyRelevance)).isEnabled());
-        }
-
-
-        @Test
-        public void testItem() throws InterruptedException
-        {
-            Thread.sleep(2000);
-            openStationary(Accessories);
-            Thread.sleep(2000);
-            Item(MountainFoxNotebook);
-           // assertEquals(driver.getCurrentUrl(), "http://40.76.27.113:8085/en/stationery/16-28-mountain-fox-notebook.html#/22-paper_type-ruled");
-        }
-
-
-
-
-
-        /*@Test
-
-        public void testqView() throws InterruptedException
-        {
-            Thread.sleep(2000);
-            WebElement Element = driver.findElement(By.xpath(Newpage));
-            js.executeScript("arguments[0].scrollIntoView();", Element);
-            Thread.sleep(2000);
-            clickView(MountainFoxNotebook);
-
-        }
-
-        @Test
-        public void TestDescription() throws InterruptedException
-        {
-            Description();
-            Thread.sleep(1000);
-            Assertions.assertTrue(driver.findElement(By.xpath(Mountain)).isDisplayed());
-        }*/
+        Thread.sleep(2000);
+        openStationary(Accessories);
+        Thread.sleep(2000);
+        assertTrue(driver.findElement(By.xpath(paperType)).isEnabled());
     }
+
+    @Test
+    public void testDropDown() throws InterruptedException
+    {
+        Thread.sleep(2000);
+        openStationary(Accessories);
+        Thread.sleep(2000);
+        selectDropDown(SortByRelevance, 1);
+        assertTrue(driver.findElement(By.xpath(SortByRelevance)).isEnabled());
+    }
+
+
+    @Test
+    public void testzItem() throws InterruptedException
+    {
+        Thread.sleep(2000);
+        openStationary(Accessories);
+        Thread.sleep(2000);
+        item(MountainFoxNotebook);
+        assertEquals(driver.getCurrentUrl(), "http://40.76.27.113:8085/en/stationery/16-28-mountain-fox-notebook.html#/22-paper_type-ruled");
+    }
+
+    @Test
+    public void testzQuickView() throws InterruptedException
+    {
+        Thread.sleep(2000);
+        openStationary(Accessories);
+        WebElement Element = driver.findElement(By.xpath(view));
+        js.executeScript("arguments[0].scrollIntoView();", Element);
+        Thread.sleep(2000);
+        quickView(MountainFoxNotebook);
+    }
+
+    @Test
+    public void testDescription() throws InterruptedException
+    {
+        openStationary(Accessories);
+        Thread.sleep(1000);
+        WebElement Element = driver.findElement(By.xpath(view));
+        js.executeScript("arguments[0].scrollIntoView();", Element);
+        Thread.sleep(2000);
+        description(MountainFoxNotebook);
+        Thread.sleep(1000);
+        Assertions.assertTrue(driver.findElement(By.xpath(mountain)).isDisplayed());
+    }
+   /* @Test
+    public void testShare() throws InterruptedException
+    {
+        int wCount2 = driver.getWindowHandles().size();
+        Thread.sleep(2000);
+        openStationary(Accessories);
+        Thread.sleep(2000);
+        item(MountainFoxNotebook);
+        Thread.sleep(2000);
+        clickShare(twitter);
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+        int currentSize2 = driver.getWindowHandles().size();
+        int expectSize2 = wCount2 + 1;
+        Assertions.assertEquals(expectSize2, currentSize2);
+
+    }*/
+}
